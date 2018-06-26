79604123e94bd19e7819709a2bf3dda96a975646
YuKitAs <moncher.merci@gmail.com>
2018-06-27 00:29:17 +0200

    Generate random access code content

--- server/src/main/java/edu/kit/pse/fridget/server/services/AccessCodeServiceImpl.java
+++ server/src/main/java/edu/kit/pse/fridget/server/services/AccessCodeServiceImpl.java
@@ -3,6 +3,9 @@ package edu.kit.pse.fridget.server.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Random;
+import java.util.stream.IntStream;
+
 import edu.kit.pse.fridget.server.models.AccessCode;
 import edu.kit.pse.fridget.server.repositories.AccessCodeRepository;
 
@@ -17,7 +20,18 @@ public class AccessCodeServiceImpl implements AccessCodeService {
 
     @Override
     public AccessCode generateAccessCode(String flatshareId) {
-        String dummyAccessCode = "ab35e";
-        return repository.save(AccessCode.buildNew(dummyAccessCode, flatshareId));
+        return repository.save(AccessCode.buildNew(generateRandomContent(), flatshareId));
+    }
+
+    private String generateRandomContent() {
+        String UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String LOWER = UPPER.toLowerCase();
+        String DIGITS = "0123456789";
+        String ALPHANUM = UPPER + LOWER + DIGITS;
+
+        StringBuilder sb = new StringBuilder();
+        IntStream.range(0, 5).forEach(i -> sb.append(ALPHANUM.charAt(new Random().nextInt(ALPHANUM.length()))));
+
+        return sb.toString();
     }
 }
