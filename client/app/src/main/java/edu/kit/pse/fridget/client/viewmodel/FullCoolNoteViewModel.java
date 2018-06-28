19ccae4dec08ec5db0ce74f51f2ce5ed2c61d697
Alina Shah <38914105+alinamon@users.noreply.github.com>
2018-06-28 11:40:51 +0200

    Corrected name

--- client/app/src/main/java/edu/kit/pse/fridget/client/viewmodel/FullCoolNoteViewModel.java
+++ client/app/src/main/java/edu/kit/pse/fridget/client/viewmodel/FullCoolNoteViewModel.java
@@ -2,5 +2,5 @@ package edu.kit.pse.fridget.client.viewmodel;
 
 import android.arch.lifecycle.ViewModel;
 
-public class FullTextCoolNoteViewModel extends ViewModel {
+public class FullCoolNoteViewModel extends ViewModel {
 }
