1df8a373a7ef61c632e6ce7cba9987f41da0281e
Alina Shah <38914105+alinamon@users.noreply.github.com>
2018-06-30 22:11:58 +0200

    Implemented onCreate method for FullTextCoolNoteActivity

--- client/app/src/main/java/edu/kit/pse/fridget/client/activity/FullTextCoolNoteActivity.java
+++ client/app/src/main/java/edu/kit/pse/fridget/client/activity/FullTextCoolNoteActivity.java
@@ -1,6 +1,19 @@
 package edu.kit.pse.fridget.client.activity;
 
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import edu.kit.pse.fridget.client.R;
+import edu.kit.pse.fridget.client.databinding.FullTextCoolNoteActivityBinding;
+
 public class FullTextCoolNoteActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        FullTextCoolNoteActivityBinding binding = DataBindingUtil.setContentView(this, R.layout.full_text_cool_note_activity);
+    }
 }
