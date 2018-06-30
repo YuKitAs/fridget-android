0de6f58912e491a464121aa06cb914ecdca33e61
Alina Shah <38914105+alinamon@users.noreply.github.com>
2018-06-30 05:16:21 +0200

    Added attributes to the member data model

--- client/app/src/main/java/edu/kit/pse/fridget/client/datamodel/Member.java
+++ client/app/src/main/java/edu/kit/pse/fridget/client/datamodel/Member.java
@@ -1,4 +1,31 @@
 package edu.kit.pse.fridget.client.datamodel;
 
+/**
+ * Member data model for a member of the flat share
+ *
+ * @author Alina Shah
+ * @version 1.0
+ */
 public class Member {
+
+    /**
+     * Id of the member
+     */
+    private String id;
+
+    /**
+     * Id of the user
+     */
+    private String userId;
+
+    /**
+     * Id of the flatshare
+     */
+    private String flatShareId;
+
+    /**
+     * Color of the member's magnet
+     */
+    private String magnetColor;
+
 }
