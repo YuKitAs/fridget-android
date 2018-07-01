6a2c6d968af8391acb19321b2f257b8d5f8a6520
Your Name <jasmin_jat@yahoo.de>
2018-07-01 23:25:24 +0200

    Login.xml angepasst

--- client/app/src/main/java/edu/kit/pse/fridget/client/activity/LoginActivity.java
+++ client/app/src/main/java/edu/kit/pse/fridget/client/activity/LoginActivity.java
@@ -4,8 +4,6 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-
 import edu.kit.pse.fridget.client.R;
 import edu.kit.pse.fridget.client.databinding.FullTextCoolNoteActivityBinding;
 
