15b4c46aee62d07e4e0daabb164b88810e43bd14
Your Name <jasmin_jat@yahoo.de>
2018-07-01 20:38:51 +0200

    Login.xml

--- client/app/src/main/java/edu/kit/pse/fridget/client/activity/LoginActivity.java
+++ client/app/src/main/java/edu/kit/pse/fridget/client/activity/LoginActivity.java
@@ -1,6 +1,20 @@
 package edu.kit.pse.fridget.client.activity;
 
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import edu.kit.pse.fridget.client.R;
+import edu.kit.pse.fridget.client.databinding.FullTextCoolNoteActivityBinding;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        FullTextCoolNoteActivityBinding binding = DataBindingUtil.setContentView(this, R.layout.login);
+    }
 }
