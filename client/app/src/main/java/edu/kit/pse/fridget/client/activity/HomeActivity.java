719de2bc712133be326bc2f05360d9e7e319a0ac
liisha <39101424+liisha@users.noreply.github.com>
2018-07-01 23:34:58 +0200

    Add files via upload

--- client/app/src/main/java/edu/kit/pse/fridget/client/activity/HomeActivity.java
+++ client/app/src/main/java/edu/kit/pse/fridget/client/activity/HomeActivity.java
@@ -1,6 +1,19 @@
 package edu.kit.pse.fridget.client.activity;
 
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import edu.kit.pse.fridget.client.R;
+import edu.kit.pse.fridget.client.databinding.FullTextCoolNoteActivityBinding;
+
 public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.home_activity);
+    }
 }
