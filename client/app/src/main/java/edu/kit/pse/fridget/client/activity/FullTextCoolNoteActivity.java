be976f75f7c58356a27695e70bf5924aaec6e35d
Alina Shah <38914105+alinamon@users.noreply.github.com>
2018-07-01 17:12:38 +0200

    Added life cycle methods to FullTextCoolNoteActivity

--- client/app/src/main/java/edu/kit/pse/fridget/client/activity/FullTextCoolNoteActivity.java
+++ client/app/src/main/java/edu/kit/pse/fridget/client/activity/FullTextCoolNoteActivity.java
@@ -5,15 +5,49 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import edu.kit.pse.fridget.client.R;
 import edu.kit.pse.fridget.client.databinding.FullTextCoolNoteActivityBinding;
 
 public class FullTextCoolNoteActivity extends AppCompatActivity {
 
+    private static final String TAG        = FullTextCoolNoteActivity.class.getSimpleName();
+
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i(TAG, "Calling onCreate");
         FullTextCoolNoteActivityBinding binding = DataBindingUtil.setContentView(this, R.layout.full_text_cool_note_activity);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "Calling onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "Calling onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        Log.i(TAG, "Calling onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.i(TAG, "Calling onStop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.i(TAG, "Calling onDestroy");
+        super.onDestroy();
     }
 }
