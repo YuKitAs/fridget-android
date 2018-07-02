ca0d26e3521a07bdfc2247871a1d747b7ba31269
liisha <39101424+liisha@users.noreply.github.com>
2018-07-02 18:40:01 +0200

    add data binding

--- client/app/src/main/java/edu/kit/pse/fridget/client/activity/HomeActivity.java
+++ client/app/src/main/java/edu/kit/pse/fridget/client/activity/HomeActivity.java
@@ -4,9 +4,8 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-
 import edu.kit.pse.fridget.client.R;
-import edu.kit.pse.fridget.client.databinding.FullTextCoolNoteActivityBinding;
+import edu.kit.pse.fridget.client.databinding.HomeActivityBinding;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -14,6 +13,7 @@ public class HomeActivity extends AppCompatActivity {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.home_activity);
+        HomeActivityBinding binding = DataBindingUtil.setContentView(this, R.layout.home_activity);
+
     }
 }
