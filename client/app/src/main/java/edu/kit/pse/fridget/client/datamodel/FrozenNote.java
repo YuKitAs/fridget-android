31bc9813df1d9102c5e13b88d6290b9b1fc6d1f6
Unknown <ubewq@student.kit.edu>
2018-07-02 11:55:04 +0200

    Frozennote.java

--- client/app/src/main/java/edu/kit/pse/fridget/client/datamodel/FrozenNote.java
+++ client/app/src/main/java/edu/kit/pse/fridget/client/datamodel/FrozenNote.java
@@ -1,4 +1,74 @@
 package edu.kit.pse.fridget.client.datamodel;
 
 public class FrozenNote {
+    /**
+     * ID of the Frozen Note
+     */
+    private String id;
+
+    /**
+     * Title of the Frozen Note
+     */
+    private String title;
+    /**
+     * Content of the Frozen Note
+     */
+    private String content;
+
+    /**
+     * ID of the Flatshare the Frozen Note belongs to
+     */
+    private String flatShareId;
+
+    /**
+     * Position of the Frozen Note on the pinboard
+     */
+    private int position;
+
+    /**
+     * Getter for the ID
+     *
+     * @return id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * Getter for the title
+     *
+     * @return title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * Getter for the content
+     *
+     * @return content
+     */
+    public String getContent() {
+        return content;
+    }
+
+    /**
+     * Getter for the flatShareId
+     *
+     * @return flatShareId
+     */
+    public String getFlatShareId() {
+        return flatShareId;
+    }
+
+    /**
+     * Getter for position of Frozen Note
+     *
+     * @return position
+     */
+    public int getPosition() {
+        return position;
+    }
+}
+
 }
