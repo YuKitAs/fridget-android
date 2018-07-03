5fcad69cc3dd49fe35ae3e2802fa537e040563ef
liisha <39101424+liisha@users.noreply.github.com>
2018-07-03 15:40:46 +0200

    Add files via upload

--- client/app/src/main/java/edu/kit/pse/fridget/client/viewmodel/HomeViewModel.java
+++ client/app/src/main/java/edu/kit/pse/fridget/client/viewmodel/HomeViewModel.java
@@ -1,6 +1,29 @@
 package edu.kit.pse.fridget.client.viewmodel;
 
+import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.kit.pse.fridget.client.datamodel.CoolNote;
+import edu.kit.pse.fridget.client.datamodel.FrozenNote;
+
 public class HomeViewModel extends ViewModel {
+    private MutableLiveData<List<CoolNote>> liveDatacNList = new MutableLiveData<List<CoolNote>>();
+    private MutableLiveData<List<FrozenNote>> liveDatafNList = new MutableLiveData<List<FrozenNote>>();
+
+    private ArrayList<CoolNote> cNList = new ArrayList<>(3);
+    private ArrayList<FrozenNote> fNList = new ArrayList<>(9);
+
+
+    public MutableLiveData<List<CoolNote>> getLiveDatacNList() {
+        return liveDatacNList;
+    }
+
+
+    private ArrayList<FrozenNote> getfNList(){
+        return fNList;
+    }
+
 }
