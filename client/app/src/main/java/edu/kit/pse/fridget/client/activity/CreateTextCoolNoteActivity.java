9bff7f349f42bc10bcdd16480685a12165bef520
Alina Shah <38914105+alinamon@users.noreply.github.com>
2018-07-03 18:33:39 +0200

    Added data binding to CreateTextCoolNoteActivity and implemented onCreate()

--- client/app/src/main/java/edu/kit/pse/fridget/client/activity/CreateTextCoolNoteActivity.java
+++ client/app/src/main/java/edu/kit/pse/fridget/client/activity/CreateTextCoolNoteActivity.java
@@ -1,7 +1,53 @@
 package edu.kit.pse.fridget.client.activity;
 
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import edu.kit.pse.fridget.client.R;
+import edu.kit.pse.fridget.client.databinding.CreateTextCoolNoteActivityBinding;
 
 public class CreateTextCoolNoteActivity extends AppCompatActivity {
+
+    private static final String TAG = FullTextCoolNoteActivity.class.getSimpleName();
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i(TAG, "Calling onCreate");
+        CreateTextCoolNoteActivityBinding binding = DataBindingUtil.setContentView(this, R.layout.create_text_cool_note_activity);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "Calling onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "Calling onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        Log.i(TAG, "Calling onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.i(TAG, "Calling onStop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.i(TAG, "Calling onDestroy");
+        super.onDestroy();
+    }
 
 }
