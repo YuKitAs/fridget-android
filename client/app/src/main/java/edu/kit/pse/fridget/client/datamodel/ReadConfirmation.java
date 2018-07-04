ada469208981ea1d309a59275fe8ec406bafaa8f
Alina Shah <38914105+alinamon@users.noreply.github.com>
2018-07-04 20:42:47 +0200

    Added attributes and getters to ReadConfirmation data model

--- client/app/src/main/java/edu/kit/pse/fridget/client/datamodel/ReadConfirmation.java
+++ client/app/src/main/java/edu/kit/pse/fridget/client/datamodel/ReadConfirmation.java
@@ -1,4 +1,24 @@
 package edu.kit.pse.fridget.client.datamodel;
 
 public class ReadConfirmation {
+
+    private String readConfirmationId;
+
+    private String coolNoteId;
+
+    private String userId;
+
+    public String getId(){
+        return readConfirmationId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getCoolNoteId() {
+        return coolNoteId;
+    }
+
 }
+
