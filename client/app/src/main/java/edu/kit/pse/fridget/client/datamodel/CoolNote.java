7f6d1d53f4a8cf67207cd9104f0deae51f23dc92
Alina Shah <38914105+alinamon@users.noreply.github.com>
2018-07-04 20:50:14 +0200

    Removed unnecessary comments

--- client/app/src/main/java/edu/kit/pse/fridget/client/datamodel/CoolNote.java
+++ client/app/src/main/java/edu/kit/pse/fridget/client/datamodel/CoolNote.java
@@ -8,91 +8,34 @@ package edu.kit.pse.fridget.client.datamodel;
  */
 public class CoolNote {
 
-
-    /**
-     * Id of the TCN
-     */
     private String id;
-
-    /**
-     * Title of the TCN
-     */
     private String title;
-    /**
-     * Content of the TCN
-     */
     private String content;
-
-    /**
-     * Id of the flat share this
-     */
     private String flatShareId;
-
-    /**
-     * Id of the User who created this TCN
-     */
     private String creatorUserId;
-
-    /**
-     * Date on which the TCN was created
-     */
     private String createdAt;
-
-    /**
-     * Position of the TCN on the pin board
-     */
     private int position;
 
-    /**
-     * Getter for the id
-     *
-     * @return id
-     */
     public String getId() {
         return id;
     }
 
-    /**
-     * Getter for the title
-     *
-     * @return title
-     */
     public String getTitle() {
         return title;
     }
 
-    /**
-     * Getter for the content
-     *
-     * @return content
-     */
     public String getContent() {
         return content;
     }
 
-    /**
-     * Getter for the flatShareId
-     *
-     * @return flatShareId
-     */
     public String getFlatShareId() {
         return flatShareId;
     }
 
-    /**
-     * Getter for the creatorUserId
-     *
-     * @return creatorUserId
-     */
     public String getCreatorUserId() {
         return creatorUserId;
     }
 
-    /**
-     * Getter for the createdAt
-     *
-     * @return createdAt
-     */
     public String getCreatedAt() {
         return createdAt;
     }
--- client/app/src/main/java/edu/kit/pse/fridget/client/datamodel/Member.java
+++ client/app/src/main/java/edu/kit/pse/fridget/client/datamodel/Member.java
@@ -8,58 +8,23 @@ package edu.kit.pse.fridget.client.datamodel;
  */
 public class Member {
 
-    /**
-     * Id of the member
-     */
     private String id;
-
-    /**
-     * Id of the user
-     */
     private String userId;
-
-    /**
-     * Id of the flatshare
-     */
     private String flatShareId;
-
-    /**
-     * Color of the member's magnet
-     */
     private String magnetColor;
 
-    /**
-     * Getter for the id
-     *
-     * @return id
-     */
     public String getId() {
         return id;
     }
 
-    /**
-     * Getter for the userId
-     *
-     * @return userId
-     */
     public String getUserId() {
         return userId;
     }
 
-    /**
-     * Getter for the flatShareId
-     *
-     * @return flatShareId
-     */
     public String getFlatShareId() {
         return flatShareId;
     }
 
-    /**
-     * Getter for the magnetColor
-     *
-     * @return magnetColor
-     */
     public String getMagnetColor() {
         return magnetColor;
     }
