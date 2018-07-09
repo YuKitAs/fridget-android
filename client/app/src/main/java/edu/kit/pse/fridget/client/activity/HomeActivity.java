b94a1c647bb5795dcadb51d4127e2956e84d596c
liisha <39101424+liisha@users.noreply.github.com>
2018-07-09 16:31:45 +0200

    add plus Button onClick

--- client/app/src/main/java/edu/kit/pse/fridget/client/activity/HomeActivity.java
+++ client/app/src/main/java/edu/kit/pse/fridget/client/activity/HomeActivity.java
@@ -1,12 +1,15 @@
 package edu.kit.pse.fridget.client.activity;
 
 import android.arch.lifecycle.ViewModelProvider;
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
 import edu.kit.pse.fridget.client.R;
 import edu.kit.pse.fridget.client.databinding.HomeActivityBinding;
 
@@ -22,6 +25,11 @@ public class HomeActivity extends AppCompatActivity {
 
         HomeViewModel homeVM = new HomeViewModel();
         binding.setHomeVM(homeVM);
+
+    }
+
+    public void createCoolNoteView(View v){
+        startActivity(new Intent(HomeActivity.this, CreateTextCoolNoteActivity.class));
 
     }
 }
