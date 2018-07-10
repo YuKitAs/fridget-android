0e1cc78286328d3007770f6e1d23844767f4252f
liisha <39101424+liisha@users.noreply.github.com>
2018-07-10 10:15:38 +0200

    Add files via upload

--- client/app/src/main/java/edu/kit/pse/fridget/client/viewmodel/HomeViewModel.java
+++ client/app/src/main/java/edu/kit/pse/fridget/client/viewmodel/HomeViewModel.java
@@ -2,13 +2,18 @@ package edu.kit.pse.fridget.client.viewmodel;
 
 import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
+import android.content.Context;
+import android.content.Intent;
 import android.databinding.Bindable;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import edu.kit.pse.fridget.client.activity.CreateTextCoolNoteActivity;
+import edu.kit.pse.fridget.client.activity.HomeActivity;
 import edu.kit.pse.fridget.client.datamodel.CoolNote;
 import edu.kit.pse.fridget.client.datamodel.FrozenNote;
 
@@ -28,9 +33,12 @@ public class HomeViewModel extends ViewModel {
     public String t1 = "^-^";
     public CoolNote cN1 = new CoolNote();
 
+
     // Konstruktor
     public HomeViewModel(){
-        this.cNList.add(cN1);
+        cN1.setTitle("yaaay"); // zum testen
+        this.getcNList();
+        this.getfNList();
         this.liveDatacNList.setValue(this.cNList);
         this.liveDatafNList.setValue(this.fNList);
     }
@@ -41,13 +49,28 @@ public class HomeViewModel extends ViewModel {
            return false;
     }
 
-    public MutableLiveData<List<CoolNote>> getLiveDatacNList() {
-        return liveDatacNList;
+
+    public String getCNTitle(int coolNoteId){
+        return this.liveDatacNList.getValue().get(coolNoteId).getTitle();
+    }
+
+    // get methode.... hier service aufrufen
+    public ArrayList<FrozenNote> getfNList(){
+        return fNList;
     }
 
 
-    private ArrayList<FrozenNote> getfNList(){
-        return fNList;
+    // get methode.... hier service aufrufen
+    public ArrayList<CoolNote> getcNList(){
+        this.cNList.add(cN1); // nur zum testen
+        return cNList;
+    }
+
+    public void onPlusButtonClicked(View view){
+        Context context = view.getContext();
+        Intent intent = new Intent(context, CreateTextCoolNoteActivity.class);
+        context.startActivity(intent);
+
     }
 
 }
