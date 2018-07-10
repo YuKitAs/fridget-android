102dca863ce45f28d1ac5222b5ed3b560ebf7e9e
liisha <39101424+liisha@users.noreply.github.com>
2018-07-10 15:39:34 +0200

    Add files via upload

--- client/app/src/main/java/edu/kit/pse/fridget/client/activity/HomeActivity.java
+++ client/app/src/main/java/edu/kit/pse/fridget/client/activity/HomeActivity.java
@@ -1,44 +1,78 @@
 package edu.kit.pse.fridget.client.activity;
 
-import android.arch.lifecycle.ViewModelProvider;
-import android.content.Intent;
+import android.arch.lifecycle.LifecycleOwner;
+
 import android.databinding.DataBindingUtil;
-import android.graphics.Color;
-import android.graphics.drawable.GradientDrawable;
+
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
+
 
 import edu.kit.pse.fridget.client.R;
 import edu.kit.pse.fridget.client.databinding.HomeActivityBinding;
 
 import edu.kit.pse.fridget.client.viewmodel.HomeViewModel;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements LifecycleOwner {
+
+    private static final String TAG = HomeActivity.class.getSimpleName();
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i(TAG, "Calling onCreate");
 
         HomeActivityBinding binding = DataBindingUtil.setContentView(this, R.layout.home_activity);
 
         HomeViewModel homeVM = new HomeViewModel();
+//        HomeViewModel homeVM = ViewModelProviders.of(this).get(HomeViewModel.class);
         binding.setHomeVM(homeVM);
 
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "Calling onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "Calling onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        Log.i(TAG, "Calling onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.i(TAG, "Calling onStop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.i(TAG, "Calling onDestroy");
+        super.onDestroy();
+    }
+
 /*
     public void createCoolNoteView(View v){
         startActivity(new Intent(HomeActivity.this, CreateTextCoolNoteActivity.class));
 
-    } */
+    }
 
     // Problem: woher weiß FullTextCoolNoteActivity welche coolNote er öffnen soll..
    public void openFullCoolNote(View v) {
         int cNNumber = Integer.parseInt(v.getTag().toString());
         startActivity(new Intent(HomeActivity.this, FullTextCoolNoteActivity.class));
+    } */
 
 
-    }
 }
