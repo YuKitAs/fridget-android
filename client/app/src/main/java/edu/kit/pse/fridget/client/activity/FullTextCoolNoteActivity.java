e6cfa1d039397438318e7e982c62c99d1aace933
Alina Shah <38914105+alinamon@users.noreply.github.com>
2018-07-12 01:08:07 +0200

    Data binding of Cool Note and ViewModel

--- client/app/src/main/java/edu/kit/pse/fridget/client/activity/FullTextCoolNoteActivity.java
+++ client/app/src/main/java/edu/kit/pse/fridget/client/activity/FullTextCoolNoteActivity.java
@@ -1,29 +1,40 @@
 package edu.kit.pse.fridget.client.activity;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.databinding.DataBindingUtil;
-import android.graphics.Color;
-import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.TextView;
 
 import edu.kit.pse.fridget.client.R;
 import edu.kit.pse.fridget.client.databinding.FullTextCoolNoteActivityBinding;
+import edu.kit.pse.fridget.client.datamodel.CoolNote;
+import edu.kit.pse.fridget.client.viewmodel.FullCoolNoteViewModel;
 
 public class FullTextCoolNoteActivity extends AppCompatActivity {
 
     private static final String TAG = FullTextCoolNoteActivity.class.getSimpleName();
+    CoolNote coolNote;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.i(TAG, "Calling onCreate");
         FullTextCoolNoteActivityBinding binding = DataBindingUtil.setContentView(this, R.layout.full_text_cool_note_activity);
+        final FullCoolNoteViewModel fullCoolNoteViewModel = ViewModelProviders.of(this).get(FullCoolNoteViewModel.class);
+        binding.setFullNoteVM(fullCoolNoteViewModel);
 
-        GradientDrawable background = (GradientDrawable) binding.magnet.getBackground();
-        background.setColor(Color.parseColor("#ff00ab"));
-        
+        TextView titleTV = binding.title;
+        TextView contentTV = binding.content;
+        //Intent i = getIntent();
+        //String title = i.getStringExtra("coolNoteTitle");
+        //String content = i.getStringExtra("coolNoteContent");
+        coolNote = fullCoolNoteViewModel.getCoolNote();
+        titleTV.setText(coolNote.getTitle());
+        contentTV.setText(coolNote.getContent());
+        binding.setCoolNote(coolNote);
     }
 
     @Override
