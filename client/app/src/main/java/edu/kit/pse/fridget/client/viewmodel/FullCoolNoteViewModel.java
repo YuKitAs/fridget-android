5ea1bcef6ae8cfc4fb3581c5bb34887ffa3144a4
Alina Shah <38914105+alinamon@users.noreply.github.com>
2018-07-12 01:11:11 +0200

    Added fake data and some methods

--- client/app/src/main/java/edu/kit/pse/fridget/client/viewmodel/FullCoolNoteViewModel.java
+++ client/app/src/main/java/edu/kit/pse/fridget/client/viewmodel/FullCoolNoteViewModel.java
@@ -1,6 +1,40 @@
 package edu.kit.pse.fridget.client.viewmodel;
 
 import android.arch.lifecycle.ViewModel;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.view.View;
+
+import edu.kit.pse.fridget.client.activity.HomeActivity;
+import edu.kit.pse.fridget.client.datamodel.CoolNote;
 
 public class FullCoolNoteViewModel extends ViewModel {
+
+    String tempMagnetColor = "#00FF00";
+    String title = getCoolNote().getTitle();
+
+    public int getMagnetColor() {
+        return Color.parseColor(tempMagnetColor);
+    }
+
+    //eigentlich vom server
+    public CoolNote getCoolNote(){
+        CoolNote tempCN = new CoolNote("a6373664-7b90-11e8-adc0-fa7ae01bbeb", "Hello World!", "Hello!","a6373664-7b90-11e8-adc0-fa7ae01bbebc",
+                "3879a0a8-546d-41cb-b26e-eb1324d0e72c", "01.01.2018", 4);
+        return tempCN;
+    }
+
+    public void deleteCoolNote(String userId, String flatshareId) {
+
+    }
+
+    public void goBack(View v) {
+
+        Context context = v.getContext();
+        Intent i = new Intent(context, HomeActivity.class);
+        context.startActivity(i);
+
+    }
+
 }
