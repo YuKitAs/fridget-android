c9fbe19928f450862df84b377171a4042fadbecd
Your Name <jasmin_jat@yahoo.de>
2018-07-14 21:53:48 +0200

    creste flatshare

--- client/app/src/main/java/edu/kit/pse/fridget/client/activity/CreateFlatshareActivity.java
+++ client/app/src/main/java/edu/kit/pse/fridget/client/activity/CreateFlatshareActivity.java
@@ -5,9 +5,15 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import edu.kit.pse.fridget.client.R;
 import edu.kit.pse.fridget.client.databinding.CreateFlatshareActivityBinding;
+import edu.kit.pse.fridget.client.datamodel.Flatshare;
+import edu.kit.pse.fridget.client.datamodel.User;
+import edu.kit.pse.fridget.client.datamodel.command.CreateFlatshareCommand;
 import edu.kit.pse.fridget.client.viewmodel.CreateFlatshareViewModel;
 
 public class CreateFlatshareActivity extends AppCompatActivity {
@@ -18,42 +24,28 @@ public class CreateFlatshareActivity extends AppCompatActivity {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.i(TAG, "Calling onCreate");
-        CreateFlatshareActivityBinding binding = DataBindingUtil.setContentView(this,R.layout.create_flatshare_activity);
-        CreateFlatshareViewModel createFlatshare = new CreateFlatshareViewModel();
-        binding.setCreateFlatshare(createFlatshare);
+        setContentView(R.layout.create_flatshare_activity);
 
+        // CreateFlatshareActivityBinding binding = DataBindingUtil.setContentView(this, R.layout.create_flatshare_activity);
+        //CreateFlatshareViewModel createFlatshare = new CreateFlatshareViewModel();
+        // binding.setCreateFlatshare(createFlatshare);
+        final EditText flatsharename = (EditText) findViewById(R.id.flatsharename_input);
+        final String user = "blabla";
+        final CreateFlatshareViewModel createFlatshareViewModel = new CreateFlatshareViewModel();
+        Button createFlatshareButton = (Button) findViewById(R.id.create);
+
+        createFlatshareButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                CreateFlatshareCommand createFlatshareCommand = new CreateFlatshareCommand(flatsharename.getText().toString(), user);
+                createFlatshareViewModel.sendNetworkRequest(createFlatshareCommand);
+
+            }
+
+
+        }) ;
 
     }
-
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.i(TAG, "Calling onStart");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.i(TAG, "Calling onResume");
-    }
-
-    @Override
-    protected void onPause() {
-        Log.i(TAG, "Calling onPause");
-        super.onPause();
-    }
-
-    @Override
-    protected void onStop() {
-        Log.i(TAG, "Calling onStop");
-        super.onStop();
-    }
-
-    @Override
-    protected void onDestroy() {
-        Log.i(TAG, "Calling onDestroy");
-        super.onDestroy();
-    }
-
 }
+
--- client/app/src/main/java/edu/kit/pse/fridget/client/viewmodel/CreateFlatshareViewModel.java
+++ client/app/src/main/java/edu/kit/pse/fridget/client/viewmodel/CreateFlatshareViewModel.java
@@ -3,16 +3,60 @@ package edu.kit.pse.fridget.client.viewmodel;
 import android.arch.lifecycle.ViewModel;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+
+import edu.kit.pse.fridget.client.R;
+import edu.kit.pse.fridget.client.activity.CreateFlatshareActivity;
 import edu.kit.pse.fridget.client.activity.StartActivity;
+import edu.kit.pse.fridget.client.datamodel.Flatshare;
+import edu.kit.pse.fridget.client.datamodel.command.CreateFlatshareCommand;
+import edu.kit.pse.fridget.client.service.FlatshareService;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 
 public class CreateFlatshareViewModel extends ViewModel {
 
-    public void onClickCreate(View v){
 
-        Context context = v.getContext();
-        Intent intent = new Intent(context, StartActivity.class);
-        context.startActivity(intent);
+    // public void onClickCreate(View v){
 
+    // sendNetworkRequest();
+
+    //}
+    private static Retrofit retrofit;
+
+
+    public void sendNetworkRequest(CreateFlatshareCommand createFlatshareCommand) {
+        //create Retrofit instance
+
+        Retrofit.Builder builder =new Retrofit.Builder()
+                .baseUrl("http://10.0.2.2:8080")
+                .addConverterFactory(GsonConverterFactory.create());
+
+        Retrofit retrofit = builder.build();
+
+        //get client & call object for the request
+        FlatshareService flatshareService =retrofit.create(FlatshareService.class);
+        Call<Flatshare> call =flatshareService.createFlatshare(createFlatshareCommand);
+        call.enqueue(new Callback<Flatshare>() {
+            @Override
+            public void onResponse(Call<Flatshare> call, Response<Flatshare> response) {
+                Log.i("sendPost", String.format("Post %s created.", new Gson().toJson(response.body())));
+            }
+
+            @Override
+            public void onFailure(Call<Flatshare> call, Throwable t) {
+                Log.e("sendPost", "Creating post failed.");
+                t.printStackTrace();
+            }
+        });
     }
 }
