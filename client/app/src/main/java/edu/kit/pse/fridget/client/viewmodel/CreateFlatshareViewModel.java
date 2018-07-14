249f1cc1b67b7e6690f9be6a60bf074488437fe9
Your Name <jasmin_jat@yahoo.de>
2018-07-15 00:09:19 +0200

    ö

--- client/app/src/main/java/edu/kit/pse/fridget/client/viewmodel/CreateFlatshareViewModel.java
+++ client/app/src/main/java/edu/kit/pse/fridget/client/viewmodel/CreateFlatshareViewModel.java
@@ -54,7 +54,7 @@ public class CreateFlatshareViewModel extends ViewModel {
 
             @Override
             public void onFailure(Call<Flatshare> call, Throwable t) {
-                Log.e("createFlatshare", "Creating flatshare failed.");
+                Log.e("createFlatshare", "Ceating flatshare failed.");
                 t.printStackTrace();
             }
         });
