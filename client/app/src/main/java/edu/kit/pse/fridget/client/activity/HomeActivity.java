2f9c552e621ec1811b788b6b202bd8caa59ca04b
liisha <39101424+liisha@users.noreply.github.com>
2018-07-15 16:09:15 +0200

    Add files via upload

--- client/app/src/main/java/edu/kit/pse/fridget/client/activity/HomeActivity.java
+++ client/app/src/main/java/edu/kit/pse/fridget/client/activity/HomeActivity.java
@@ -1,25 +1,14 @@
 package edu.kit.pse.fridget.client.activity;
 
 import android.arch.lifecycle.LifecycleOwner;
-
-import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.databinding.DataBindingUtil;
-
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-
-
 import edu.kit.pse.fridget.client.R;
 import edu.kit.pse.fridget.client.databinding.HomeActivityBinding;
-
-import edu.kit.pse.fridget.client.datamodel.CoolNote;
-import edu.kit.pse.fridget.client.datamodel.FrozenNote;
-import edu.kit.pse.fridget.client.datamodel.Member;
 import edu.kit.pse.fridget.client.viewmodel.HomeViewModel;
 
 public class HomeActivity extends AppCompatActivity implements LifecycleOwner {
@@ -41,31 +30,13 @@ public class HomeActivity extends AppCompatActivity implements LifecycleOwner {
 
         binding.setLifecycleOwner(this);
 
-        /*// stupide Art und Weise um alles zu refreshen...
-        ((Button) findViewById(R.id.coolNote1)).setText(homeVM.getCNTitle(1));
-        ((Button) findViewById(R.id.cNmagnet1)).setBackgroundColor(homeVM.getMagnetColor(1)); // anders... mit Color statelist und so...
-        ((Button) findViewById(R.id.coolNote1)).setVisibility(homeVM.isEmptyCN2(1)); // klappt nicht wirklich... nur text verschwindet
-        ((Button) findViewById(R.id.cNmagnet1)).setVisibility(homeVM.isEmptyCN2(1));
-
-
-        Observer<CoolNote[]> cnListObserver = new Observer<CoolNote[]>() {
-            @Override
-            public void onChanged(@Nullable CoolNote[] coolNotes) {
-                Button cn1 = (Button) findViewById(R.id.coolNote1);
-                cn1.setVisibility(homeVM.isEmptyCN2(1));
-                cn1.setText(homeVM.getCNTitle(1));
-            }
-        };
-
-        homeVM.getLiveDataCNList().observe(this,cnListObserver);
-*/
-
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         Log.i(TAG, "Calling onStart");
+        homeVM.updateLists();
     }
 
     @Override
