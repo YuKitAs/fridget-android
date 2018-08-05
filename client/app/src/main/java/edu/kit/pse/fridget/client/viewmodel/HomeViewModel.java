c3e77f3df9037ca8e86de6bf0592fee3d7ec378a
liisha <39101424+liisha@users.noreply.github.com>
2018-08-05 17:09:15 +0200

    add retrofit stuff

--- client/app/src/main/java/edu/kit/pse/fridget/client/viewmodel/HomeViewModel.java
+++ client/app/src/main/java/edu/kit/pse/fridget/client/viewmodel/HomeViewModel.java
@@ -5,9 +5,12 @@ import android.arch.lifecycle.ViewModel;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Random;
 
 import edu.kit.pse.fridget.client.activity.CreateTextCoolNoteActivity;
@@ -17,6 +20,13 @@ import edu.kit.pse.fridget.client.activity.MenuDrawerActivity;
 import edu.kit.pse.fridget.client.datamodel.CoolNote;
 import edu.kit.pse.fridget.client.datamodel.FrozenNote;
 import edu.kit.pse.fridget.client.datamodel.Member;
+import edu.kit.pse.fridget.client.service.CoolNoteService;
+import edu.kit.pse.fridget.client.service.FrozenNoteService;
+import edu.kit.pse.fridget.client.service.MembershipService;
+import edu.kit.pse.fridget.client.service.RetrofitClientInstance;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Diese Klasse stellt alle benötigten Informationen zur Verfügung, die die HomeActivity benötigt
@@ -40,6 +50,7 @@ public class HomeViewModel extends ViewModel {
     private Member[] memberList = new Member[15];
     private Integer[] magnetColorList = new Integer[9];
 
+    private String flatshareId;
 
     /**
      * Konstruktor
@@ -48,21 +59,29 @@ public class HomeViewModel extends ViewModel {
         this.fakeCN(); // wird gelöscht
         this.fakeFN(); // wird gelöscht
         this.fakeMembers(); // wird gelöscht
-        this.setLiveDataVisibilityList(this.cNList);
-        this.setLiveDataMagnetColorList(this.getMemberList());
+
+        this.setFlatsharIDFromSP();
+       // this.setLiveDataVisibilityList(this.cNList);
+       // this.setLiveDataMagnetColorList(this.getMemberList());
         updateLists();
+    }
+
+    private void setFlatsharIDFromSP() {
+        String id = null;
+
+        this.flatshareId = id;
+    }
+
+    private String getFlatshareId() {
+        return this.flatshareId;
     }
 
     /**
      * alle Listen werden aus dem Server neugeholt
      */
     public void updateLists() {
-
         this.liveDataCNList.setValue(this.getcNList());
-        this.liveDataVisibilityList.setValue(this.visibilityList);
         this.liveDataFNList.setValue(this.getfNList());
-        this.liveDataMagnetColorList.setValue(this.magnetColorList);
-
     }
 
     /**
@@ -72,7 +91,29 @@ public class HomeViewModel extends ViewModel {
      * @return
      */
     public FrozenNote[] getfNList() {
+        RetrofitClientInstance.getRetrofitInstance().create(FrozenNoteService.class).getAllFrozenNote(flatshareId).enqueue(new Callback<List<FrozenNote>>() {
+            @Override
+            public void onResponse(Call<List<FrozenNote>> call, Response<List<FrozenNote>> response) {
+                //fNList darf nie null sein, da immer FrozenNotes existieren
+                fNList = null;
 
+                List<FrozenNote> frozenNotes = response.body();
+                if (frozenNotes != null) {
+                    // Liste in ein nach der Position geordneten Array umwandeln
+                    for (FrozenNote fn : frozenNotes) {
+                        fNList[fn.getPosition() - 1] = fn;
+                    }
+                } else {
+                    Log.e("getfNList1", "There are no Frozen Note.");
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<FrozenNote>> call, Throwable t) {
+
+            }
+        });
         return fNList;
     }
 
@@ -85,6 +126,30 @@ public class HomeViewModel extends ViewModel {
      */
     public CoolNote[] getcNList() {
 
+
+        RetrofitClientInstance.getRetrofitInstance().create(CoolNoteService.class).getAllCoolNotes(flatshareId).enqueue(new Callback<List<CoolNote>>() {
+            @Override
+            public void onResponse(Call<List<CoolNote>> call, Response<List<CoolNote>> response) {
+                cNList = null;
+                List<CoolNote> coolNotes = response.body();
+
+                if (coolNotes != null) {
+                    // Liste in ein nach der Position geordneten Array umwandeln
+                    for (CoolNote cn : coolNotes) {
+                        cNList[coolNotes.get(0).getPosition() - 1] = cn;
+                    }
+                } else {
+                    Log.e("getcNList1", "There are no Cool Note.");
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<CoolNote>> call, Throwable t) {
+                Log.e("getcNList2", "Get CoolNoteList failed.");
+
+            }
+        });
 
         // visibiltyList updaten
         this.setLiveDataVisibilityList(this.cNList);
@@ -100,6 +165,28 @@ public class HomeViewModel extends ViewModel {
      */
     public Member[] getMemberList() {
 
+        RetrofitClientInstance.getRetrofitInstance().create(MembershipService.class).getMemberList(flatshareId).enqueue(new Callback<List<Member>>() {
+            @Override
+            public void onResponse(Call<List<Member>> call, Response<List<Member>> response) {
+                memberList = null;
+                List<Member> members = response.body();
+
+                if (members != null) {
+                    for (int m = 0; m < 15; m++) {
+                        memberList[m] = members.get(m);
+                    }
+                } else {
+                    Log.e("getmemberList1", "There are no Members.");
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Member>> call, Throwable t) {
+
+            }
+        });
+
         return memberList;
     }
 
@@ -108,18 +195,31 @@ public class HomeViewModel extends ViewModel {
         CoolNote[] cList = this.cNList;
 
         int i = 0;
-        for (CoolNote cn : cList) {
-            if (cn != null) {
-                this.magnetColorList[i] = getMemberColorbyUserId(cn.getCreatorMembershipId(), memberlist);
-            } else {
-                this.magnetColorList[i] = Color.parseColor("#FFFFFF"); // wird invisible gesetzt... also egal, welche Farbe
+        if (cNList != null) {
+            for (CoolNote cn : cList) {
+                if (cn != null) {
+                    this.magnetColorList[i] = getMemberColorbyUserId(cn.getCreatorMembershipId(), memberlist);
+                } else {
+                    this.magnetColorList[i] = Color.parseColor("#FFFFFF"); // wird invisible gesetzt... also egal, welche Farbe
+                }
+                i++;
             }
-            i++;
+        } else {
+            for (int n = 0; n < 9; n++) {
+                this.magnetColorList[n] = Color.parseColor("#FFFFFF");
+            }
         }
+
+        this.liveDataMagnetColorList.setValue(this.magnetColorList);
     }
 
     private int getMemberColorbyUserId(String id, Member[] memberlist) {
         Member[] mlist = memberlist;
+
+        if (mlist == null) {
+            Log.e("member","The member is not found");
+            return Color.parseColor("#FFFFFF"); // sollte nie erreicht werden... sonst heißt es, dass es den Member nicht gibt
+        }
 
         for (Member m : mlist) {
             if (m.getUserId().equals(id)) {
@@ -127,18 +227,23 @@ public class HomeViewModel extends ViewModel {
             }
         }
         return Color.parseColor("#FFFFFF"); // sollte nie erreicht werden... sonst heißt es, dass es den Member nicht gibt
+
     }
 
 
     public void setLiveDataVisibilityList(CoolNote[] list) {
         int i = 0;
-        for (CoolNote cn : list) {
-            if (cn != null) {
-                this.visibilityList[i] = true;
-            } else {
-                this.visibilityList[i] = false;
+        if (list != null) {
+            for (CoolNote cn : list) {
+                if (cn != null) {
+                    this.visibilityList[i] = true;
+                } else {
+                    this.visibilityList[i] = false;
+                }
+                i++;
             }
-            i++;
+        } else {
+            this.visibilityList = null;
         }
 
         this.liveDataVisibilityList.setValue(this.visibilityList);
@@ -153,7 +258,7 @@ public class HomeViewModel extends ViewModel {
         LinkedList<Integer> emptyPositions = this.getListOfEmptySpaceForCoolNote();
         if (emptyPositions == null) {
             // Fehlermeldung... Man kann keine weiteren CoolNotes mehr einfügen
-
+            Toast.makeText(view.getContext(), "You can't add more Cool Note. Please delete some CoolNotes.",Toast.LENGTH_SHORT).show();
 
         } else {
             int position = this.getRandomEmptyPosition();
@@ -161,13 +266,6 @@ public class HomeViewModel extends ViewModel {
             Intent intent = new Intent(context, CreateTextCoolNoteActivity.class);
             intent.putExtra("position", position);
             context.startActivity(intent);
-
-            //testen
-            CoolNote cN3 = new CoolNote("3", "lelele", "testtest",
-                    "2", 3, 1, null, null);
-            this.cNList[position] = cN3;
-
-            this.updateLists();
         }
 
     }
@@ -180,14 +278,12 @@ public class HomeViewModel extends ViewModel {
     }
 
     public void onRefreshButtonClicked(View view) {
-        // testen
-       /* LinkedList<Integer> emptyPositions = this.getListOfEmptySpaceForCoolNote();
-        int position = this.getRandomEmptyPosition(emptyPositions);
-        CoolNote cN4 = new CoolNote("4","dsfgdfg","testtest, sdfgsdfg",
-                "0","2","10072018",2);
-        this.cNList[position]=cN4;
-        updateLists(); */
+        /*//testen
+        int position = this.getRandomEmptyPosition();
 
+        CoolNote cN3 = new CoolNote("3", "piep", "testtest",
+                "2", 3, 1, null, null);
+        this.cNList[position] = cN3; */
 
         updateLists();
 
@@ -200,14 +296,21 @@ public class HomeViewModel extends ViewModel {
      * @param view
      */
     public void openFullCoolNote(View view) {
-        Context context = view.getContext();
-        int position = Integer.parseInt(view.getTag().toString());
-        String cNid = this.liveDataCNList.getValue()[position - 1].getId();
-        Intent intent = new Intent(context, FullTextCoolNoteActivity.class);
-        intent.putExtra("coolNoteId", cNid);
-        context.startActivity(intent);
-
         this.updateLists();
+        int position = Integer.parseInt(view.getTag().toString());
+        if (liveDataCNList.getValue() != null ) {
+            if (this.liveDataCNList.getValue()[position - 1] != null) {
+                Context context = view.getContext();
+                String cNid = this.liveDataCNList.getValue()[position - 1].getId();
+                Intent intent = new Intent(context, FullTextCoolNoteActivity.class);
+                intent.putExtra("coolNoteId", cNid);
+                context.startActivity(intent);
+            } else {
+                Toast.makeText(view.getContext(),"This Cool Note was deleted.",Toast.LENGTH_SHORT).show();
+            }
+        }else {
+            Toast.makeText(view.getContext(),"This Cool Note was deleted.",Toast.LENGTH_SHORT).show();
+        }
     }
 
     /**
@@ -216,14 +319,21 @@ public class HomeViewModel extends ViewModel {
      * @param view
      */
     public void openFullFrozenNote(View view) {
+        this.updateLists();
         Context context = view.getContext();
         int position = Integer.parseInt(view.getTag().toString());
-        String fNid = this.liveDataFNList.getValue()[position - 1].getId();
-        Intent intent = new Intent(context, FullTextFrozenNoteActivity.class);
-        intent.putExtra("frozenNoteId", fNid);
-        context.startActivity(intent);
 
-        this.updateLists();
+        if (liveDataCNList.getValue() != null) {
+            if ( this.liveDataFNList.getValue()[position - 1] != null) {
+                String fNid = this.liveDataFNList.getValue()[position - 1].getId();
+                Intent intent = new Intent(context, FullTextFrozenNoteActivity.class);
+                intent.putExtra("frozenNoteId", fNid);
+                context.startActivity(intent);
+            }
+        } else {
+            // sollte nie erreicht werden, da es immer 3 Frozen Notes gibt.
+            Log.e("frozenNote", "Get FrozenNote failed.");
+        }
     }
 
 
@@ -291,13 +401,13 @@ public class HomeViewModel extends ViewModel {
 
         this.cNList[0] = cN1;
         this.cNList[1] = cN2;
-        this.cNList[2] = null;
+        this.cNList[2] = cN3;
         this.cNList[3] = cN4;
         this.cNList[4] = cN5;
         this.cNList[5] = cN6;
         this.cNList[6] = cN7;
         this.cNList[7] = cN8;
-        this.cNList[8] = null;
+        this.cNList[8] = cN9;
 
     }
 
