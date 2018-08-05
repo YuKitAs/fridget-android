2e8d360bb34d3655cbfa86b9d218a2bf361941c4
YuKitAs <moncher.merci@gmail.com>
2018-08-05 16:57:46 +0200

    Pass coolNoteId from message payload to intent

--- client/app/src/main/java/edu/kit/pse/fridget/client/service/MyFirebaseMessagingService.java
+++ client/app/src/main/java/edu/kit/pse/fridget/client/service/MyFirebaseMessagingService.java
@@ -7,50 +7,57 @@ import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
+import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Map;
 
 import edu.kit.pse.fridget.client.R;
 import edu.kit.pse.fridget.client.activity.FullTextCoolNoteActivity;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
-
+    private static final String TAG = MyFirebaseMessagingService.class.getSimpleName();
     private NotificationManager notificationManager;
 
     public MyFirebaseMessagingService() {
     }
 
     @Override
-    public void onMessageReceived(RemoteMessage remoteMessage)
-    {
-        if(remoteMessage == null)
-        {
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        if (remoteMessage == null) {
             return;
         }
-        if(remoteMessage.getNotification() != null)
-        {
-            handleNotification(remoteMessage.getNotification().getTitle());
+
+        String coolNoteId = "0";
+        Map<String, String> payload = remoteMessage.getData();
+
+        if (payload.size() > 0) {
+            Log.d(TAG, "Message data payload: " + payload);
+            coolNoteId = payload.get("coolNoteId");
+        }
+
+        if (remoteMessage.getNotification() != null) {
+            sendNotification(remoteMessage.getNotification(), coolNoteId);
         }
     }
-    public void handleNotification(String message)
-    {
-        sendNotification(message);
-    }
 
-    public void sendNotification(String message)
-    {
+    private void sendNotification(RemoteMessage.Notification notification, String coolNoteId) {
         Intent intent = new Intent(this, FullTextCoolNoteActivity.class);
+        intent.putExtra("coolNoteId", coolNoteId);
+
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        message = "Look at this new Cool Note!";
+
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
                 .setSmallIcon(R.drawable.fridget_logo)
-                .setContentTitle("Fridget")
-                .setContentText(message)
+                .setContentTitle(notification.getTitle())
+                .setContentText(notification.getBody())
                 .setAutoCancel(true)
                 .setSound(defaultSoundUri)
                 .setContentIntent(pendingIntent);
+
         notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.notify(0, notificationBuilder.build());
     }
