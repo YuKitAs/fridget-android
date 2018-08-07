e019edd5ac66164af6584c7635a79e605574994c
Your Name <jasmin_jat@yahoo.de>
2018-08-07 18:01:46 +0200

    ActivityTests hinzugefügt

---
+++ client/app/src/androidTest/java/edu/kit/pse/fridget/client/activity/ActivityTests/EnterAccessCodeActivityTest.java
@@ -0,0 +1,43 @@
+package edu.kit.pse.fridget.client.activity.ActivityTests;
+
+import android.support.test.rule.ActivityTestRule;
+import android.view.View;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import edu.kit.pse.fridget.client.R;
+import edu.kit.pse.fridget.client.activity.EnterAccessCodeActivity;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Diese Klasse testet, ob EnterAccessCode gestartet wird
+ */
+public class EnterAccessCodeActivityTest {
+
+    @Rule
+    public ActivityTestRule<EnterAccessCodeActivity> mActivityTestRule = new ActivityTestRule<>(EnterAccessCodeActivity.class);
+
+    private EnterAccessCodeActivity enterAccessCodeActivity =null;
+
+    @Before
+    public void setUp() throws Exception {
+        enterAccessCodeActivity = mActivityTestRule.getActivity();
+    }
+
+
+    @Test
+    public void TestLaunch() {
+        View view =enterAccessCodeActivity.findViewById(R.id.enter_access_code);
+        assertNotNull(view);
+    }
+
+
+    @After
+    public void tearDown() throws Exception {
+        enterAccessCodeActivity =null;
+    }
+}
