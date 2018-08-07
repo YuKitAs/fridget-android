706a1c89b617af4ba6a99490c69a490c3519339c
YuKitAs <moncher.merci@gmail.com>
2018-08-07 22:38:34 +0200

    Change base URL for retrofit

--- client/app/src/main/java/edu/kit/pse/fridget/client/service/RetrofitClientInstance.java
+++ client/app/src/main/java/edu/kit/pse/fridget/client/service/RetrofitClientInstance.java
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClientInstance {
     private static Retrofit retrofit;
-    private static final String BASE_URL = "http://10.0.2.2:8080/";
+    private static final String BASE_URL = "http://45.77.67.122:8080/";
 
     public static Retrofit getRetrofitInstance() {
         if (retrofit == null) {
