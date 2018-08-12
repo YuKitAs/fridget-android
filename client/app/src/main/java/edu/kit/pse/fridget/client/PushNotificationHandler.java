f7bdbbf483a9b65e202c1e1ebacdf6a17ccd8263
YuKitAs <moncher.merci@gmail.com>
2018-08-12 20:07:52 +0200

    Show push notification if the App is in the foreground

--- client/app/src/main/java/edu/kit/pse/fridget/client/PushNotificationHandler.java
+++ client/app/src/main/java/edu/kit/pse/fridget/client/PushNotificationHandler.java
@@ -1,11 +1,13 @@
-package edu.kit.pse.fridget.client.service;
+package edu.kit.pse.fridget.client;
 
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
@@ -14,18 +16,14 @@ import com.google.firebase.messaging.RemoteMessage;
 
 import java.util.Map;
 
-import edu.kit.pse.fridget.client.R;
 import edu.kit.pse.fridget.client.activity.FullTextCoolNoteActivity;
 
-public class MyFirebaseMessagingService extends FirebaseMessagingService {
-    private static final String TAG = MyFirebaseMessagingService.class.getSimpleName();
-    private NotificationManager notificationManager;
-
-    public MyFirebaseMessagingService() {
-    }
+public class PushNotificationHandler extends FirebaseMessagingService {
+    private static final String TAG = PushNotificationHandler.class.getSimpleName();
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
+        Log.i(TAG, "Received remote message");
         if (remoteMessage == null) {
             return;
         }
@@ -34,7 +32,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         Map<String, String> payload = remoteMessage.getData();
 
         if (payload.size() > 0) {
-            Log.d(TAG, "Message data payload: " + payload);
+            Log.i(TAG, "Message data payload: " + payload);
             coolNoteId = payload.get("coolNoteId");
         }
 
@@ -43,14 +41,20 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         }
     }
 
-    private void sendNotification(RemoteMessage.Notification notification, String coolNoteId) {
+    public Intent getPushNotificationIntent(String coolNoteId) {
         Intent intent = new Intent(this, FullTextCoolNoteActivity.class);
         intent.putExtra("coolNoteId", coolNoteId);
+        return intent;
+    }
+
+    private void sendNotification(RemoteMessage.Notification notification, String coolNoteId) {
+        Intent intent = getPushNotificationIntent(coolNoteId);
 
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
 
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+        String channelId = getString(R.string.default_notification_channel_id);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, channelId)
                 .setSmallIcon(R.drawable.fridget_logo)
                 .setContentTitle(notification.getTitle())
                 .setContentText(notification.getBody())
@@ -58,7 +62,15 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
                 .setSound(defaultSoundUri)
                 .setContentIntent(pendingIntent);
 
-        notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(channelId,
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(channel);
+        }
+
         notificationManager.notify(0, notificationBuilder.build());
     }
 }
