6288e35bc01b12a6a7ec02848052e8f89bdd07a9
Alina Shah <38914105+alinamon@users.noreply.github.com>
2018-08-12 03:38:02 +0200

    Leave flatshare

--- client/app/src/main/java/edu/kit/pse/fridget/client/fragment/LeaveFlatshareFragment.java
+++ client/app/src/main/java/edu/kit/pse/fridget/client/fragment/LeaveFlatshareFragment.java
@@ -1,6 +1,7 @@
 package edu.kit.pse.fridget.client.fragment;
 
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -9,23 +10,18 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import edu.kit.pse.fridget.client.R;
-import edu.kit.pse.fridget.client.viewmodel.fragmentVM.LeaveFlatshareFragmentVM;
-import android.databinding.DataBindingUtil;
-
 import edu.kit.pse.fridget.client.databinding.FragmentLeaveFlatshareBinding;
+import edu.kit.pse.fridget.client.viewmodel.fragmentVM.LeaveFlatshareFragmentVM;
 
 public class LeaveFlatshareFragment extends Fragment {
 
     private static final String TAG = LeaveFlatshareFragment.class.getSimpleName();
-
     LeaveFlatshareFragmentVM vm = new LeaveFlatshareFragmentVM();
 
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        }
+    public void onViewCreated(View v, Bundle savedInstanceState) {
+        super.onViewCreated(v, savedInstanceState);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -33,7 +29,7 @@ public class LeaveFlatshareFragment extends Fragment {
         // Inflate the layout for this fragment
         Log.i(TAG, "Calling onCreateView");
         FragmentLeaveFlatshareBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_leave_flatshare, container, false);
-        //binding.setVm(vm);
+        binding.setVm(vm);
         return binding.getRoot();
     }
 
--- client/app/src/main/java/edu/kit/pse/fridget/client/viewmodel/fragmentVM/LeaveFlatshareFragmentVM.java
+++ client/app/src/main/java/edu/kit/pse/fridget/client/viewmodel/fragmentVM/LeaveFlatshareFragmentVM.java
@@ -2,16 +2,13 @@ package edu.kit.pse.fridget.client.viewmodel.fragmentVM;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 
-import com.google.gson.Gson;
-
-import java.util.List;
-
 import edu.kit.pse.fridget.client.activity.HomeActivity;
 import edu.kit.pse.fridget.client.activity.StartActivity;
-import edu.kit.pse.fridget.client.datamodel.representation.UserMembershipRepresentation;
 import edu.kit.pse.fridget.client.service.MembershipService;
 import edu.kit.pse.fridget.client.service.RetrofitClientInstance;
 import edu.kit.pse.fridget.client.viewmodel.SharedPreferencesData;
@@ -23,24 +20,18 @@ public class LeaveFlatshareFragmentVM {
     private static final String TAG = LeaveFlatshareFragmentVM.class.getSimpleName();
     private SharedPreferencesData sharedPreferencesData =new SharedPreferencesData();
 
-    private String flatshareId;
-
-    public LeaveFlatshareFragmentVM() {
-        getMemberList(flatshareId);
-    }
-
-    public void setFlatshareId(String flat) {
-        this.flatshareId = flat;
-    }
-
     public void deleteMembership(View v) {
-        String flatshareId =sharedPreferencesData.getSharedPreferencesData("flatshareId",v);
         String userId =sharedPreferencesData.getSharedPreferencesData("ownMemberId",v);
+        String flatshareId = sharedPreferencesData.getSharedPreferencesData("flatshareId", v);
         final Context context = v.getContext();
         Intent intent = new Intent(context, StartActivity.class);
         RetrofitClientInstance.getRetrofitInstance().create(MembershipService.class).deleteMember(flatshareId, userId).enqueue(new Callback<Void>() {
             @Override
             public void onResponse(Call<Void> call, Response<Void> response) {
+                SharedPreferences mySPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+                SharedPreferences.Editor editor = mySPrefs.edit();
+                editor.remove("ownMemberId");
+                editor.apply();
                 Log.i(TAG, "Member has been deleted.");
                 context.startActivity(intent);
             }
@@ -48,28 +39,6 @@ public class LeaveFlatshareFragmentVM {
             @Override
             public void onFailure(Call<Void> call, Throwable t) {
                 Log.e(TAG, "Deleting Member failed.");
-                t.printStackTrace();
-            }
-        });
-    }
-
-    public void getMemberList(String flatshareId) {
-        RetrofitClientInstance.getRetrofitInstance().create(MembershipService.class).getMemberList(flatshareId).enqueue(new Callback<List<UserMembershipRepresentation>>() {
-            @Override
-            public void onResponse(Call<List<UserMembershipRepresentation>> call, Response<List<UserMembershipRepresentation>> response) {
-                List<UserMembershipRepresentation> memList = response.body();
-                if (memList != null) {
-                    Log.i(TAG, String.format("Member list fetched: %s", new Gson().toJson(memList)));
-
-                } else {
-                    Log.e(TAG, "There are no Member.");
-
-                }
-            }
-
-            @Override
-            public void onFailure(Call<List<UserMembershipRepresentation>> call, Throwable t) {
-                Log.e(TAG, "Fetching member list failed.");
                 t.printStackTrace();
             }
         });
