897dded2bc41d6380298a870ea46ef6f7a14e7a0
Alina Shah <38914105+alinamon@users.noreply.github.com>
2018-08-12 23:38:00 +0200

    Service and viewmodel description

---
+++ client/app/src/androidTest/java/edu/kit/pse/fridget/client/activity/ActivityTests/viewmodel/CreateCoolNoteViewModelTest.java
@@ -0,0 +1,14 @@
+package edu.kit.pse.fridget.client.activity.ActivityTests.viewmodel;
+
+import edu.kit.pse.fridget.client.activity.CreateTextCoolNoteActivity;
+import edu.kit.pse.fridget.client.viewmodel.CreateCoolNoteViewModel;
+
+public class CreateCoolNoteViewModelTest {
+
+    private CreateTextCoolNoteActivity createTextCoolNoteActivity;
+
+    private CreateCoolNoteViewModel createCoolNoteViewModel;
+
+
+}
+
