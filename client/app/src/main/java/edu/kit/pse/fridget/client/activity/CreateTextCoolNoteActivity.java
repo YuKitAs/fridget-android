ce834d4658128294035f6a99625eb450750cb6b2
Alina Shah <38914105+alinamon@users.noreply.github.com>
2018-08-12 21:41:23 +0200

    navigationview in edit and create

--- client/app/src/main/java/edu/kit/pse/fridget/client/activity/CreateTextCoolNoteActivity.java
+++ client/app/src/main/java/edu/kit/pse/fridget/client/activity/CreateTextCoolNoteActivity.java
@@ -6,6 +6,9 @@ import android.content.SharedPreferences;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -13,13 +16,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 
 import edu.kit.pse.fridget.client.R;
 import edu.kit.pse.fridget.client.databinding.CreateTextCoolNoteActivityBinding;
 import edu.kit.pse.fridget.client.databinding.NavHeaderBinding;
+import edu.kit.pse.fridget.client.fragment.CreateAccessCodeFragment;
+import edu.kit.pse.fridget.client.fragment.LeaveFlatshareFragment;
+import edu.kit.pse.fridget.client.fragment.MemberListFragment;
 import edu.kit.pse.fridget.client.viewmodel.CreateCoolNoteViewModel;
 
-public class CreateTextCoolNoteActivity extends AppCompatActivity {
+public class CreateTextCoolNoteActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private static final String TAG = CreateTextCoolNoteActivity.class.getSimpleName();
 
@@ -57,6 +64,9 @@ public class CreateTextCoolNoteActivity extends AppCompatActivity {
                 .navView, false);
         binding.navView.addHeaderView(_bind.getRoot());
         _bind.flatsharename.setText(flatshareName);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
@@ -67,6 +77,38 @@ public class CreateTextCoolNoteActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        Fragment fragment = null;
+
+        if (id == R.id.memlist) {
+            fragment = new MemberListFragment();
+        } else if (id == R.id.add_member) {
+            fragment = new CreateAccessCodeFragment();
+        } else if (id == R.id.leave_flatshare) {
+            fragment = new LeaveFlatshareFragment();
+        }
+
+        //Übergeben von FlatshareId mit Bundle
+        SharedPreferences sharedPreferences = getSharedPreferences("edu.kit.pse.fridget.client_preferences", MODE_PRIVATE);
+        String fsId = sharedPreferences.getString("flatshareId", "N/A");
+        Bundle bundle = new Bundle();
+        bundle.putString("FlatshareId", fsId);
+
+        if (fragment != null) {
+            fragment.setArguments(bundle);
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.placeholder_frame, fragment);
+            setNoteInvisible();
+            ft.commit();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override
@@ -97,6 +139,10 @@ public class CreateTextCoolNoteActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         Log.i(TAG, "Calling onDestroy");
+    }
+
+    private void setNoteInvisible() {
+        findViewById(R.id.rootRL).setVisibility(View.INVISIBLE);
     }
 
 }
