a8b310a10475c58b4ede0f110bf4ef400cb660e5
YuKitAs <moncher.merci@gmail.com>
2018-08-14 01:28:49 +0200

    Revert "Viewechsel Tests hinzugefügt"
    Because it breaks the push notification redirection
    This reverts commit 344b718

--- client/app/src/main/java/edu/kit/pse/fridget/client/activity/StartActivity.java
+++ client/app/src/main/java/edu/kit/pse/fridget/client/activity/StartActivity.java
@@ -1,59 +1,43 @@
 package edu.kit.pse.fridget.client.activity;
 
-import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-
-import com.google.firebase.auth.FirebaseUser;
 
 import edu.kit.pse.fridget.client.R;
 import edu.kit.pse.fridget.client.databinding.StartActivityBinding;
-import edu.kit.pse.fridget.client.viewmodel.EnterAccessCodeViewModel;
+import edu.kit.pse.fridget.client.utility.SafeIntentExtrasExtractor;
 import edu.kit.pse.fridget.client.viewmodel.StartViewModel;
 
 public class StartActivity extends AppCompatActivity {
 
-
     private static final String TAG = StartActivity.class.getSimpleName();
-    public static final String DEFAULT="N/A";
-    Context context =this;
-
-
+    private static final String DEFAULT = "N/A";
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.i(TAG, "Calling onCreate");
+
+        // Redirect to next activity before loading the full view
+        String flatshareId = restoreFlatshareId();
+        if (!DEFAULT.equals(flatshareId)) {
+            redirectToNextActivity(flatshareId);
+            return;
+        }
+
         StartActivityBinding binding = DataBindingUtil.setContentView(this, R.layout.start_activity);
         StartViewModel start = new StartViewModel();
         binding.setStart(start);
-        SharedPreferences sharedPreferences =getSharedPreferences("edu.kit.pse.fridget.client_preferences",MODE_PRIVATE);
-        String flatshareId =sharedPreferences.getString("flatshareId", DEFAULT);
-
-        updateUI(flatshareId);
     }
 
 
     @Override
     protected void onStart() {
-
         super.onStart();
-        //Daten flatshareiD von shared preferences wird abgefragt
-        SharedPreferences sharedPreferences =getSharedPreferences("edu.kit.pse.fridget.client_preferences",MODE_PRIVATE);
-        String flatshareId =sharedPreferences.getString("flatshareId", DEFAULT);
-
-       updateUI(flatshareId);
-
-
         Log.i(TAG, "Calling onStart");
     }
 
@@ -65,27 +49,46 @@ public class StartActivity extends AppCompatActivity {
 
     @Override
     protected void onPause() {
-        Log.i(TAG, "Calling onPause");
         super.onPause();
+        Log.i(TAG, "Calling onPause");
     }
 
     @Override
     protected void onStop() {
-        Log.i(TAG, "Calling onStop");
         super.onStop();
+        Log.i(TAG, "Calling onStop");
 
     }
 
     @Override
     protected void onDestroy() {
-        Log.i(TAG, "Calling onDestroy");
         super.onDestroy();
+        Log.i(TAG, "Calling onDestroy");
     }
 
-    //Es wird geprüft ob schon eine flatshareid vorhanden ist, wenn ja wird automatisch in HomeActivity gewechselt
-    private void updateUI(String flatshareId) {
-        if (flatshareId !=DEFAULT) {
-            startActivity(new Intent(StartActivity.this, HomeActivity.class));
+    private String restoreFlatshareId() {
+        return getSharedPreferences("edu.kit.pse.fridget.client_preferences", MODE_PRIVATE)
+                .getString("flatshareId", DEFAULT);
+    }
+
+    private void redirectToNextActivity(String flatshareId) {
+        // If user clicks on the push notification, coolNoteId will be set in extras
+        String coolNoteId = SafeIntentExtrasExtractor.getString(getIntent(), "coolNoteId");
+
+        if (coolNoteId != null) {
+            redirectToFullTextCoolNoteActivity(coolNoteId);
+        } else {
+            redirectToHomeActivity();
         }
+    }
+
+    private void redirectToHomeActivity() {
+        startActivity(new Intent(StartActivity.this, HomeActivity.class));
+    }
+
+    private void redirectToFullTextCoolNoteActivity(String coolNoteId) {
+        Intent intent = new Intent(StartActivity.this, FullTextCoolNoteActivity.class);
+        intent.putExtra("coolNoteId", coolNoteId);
+        startActivity(intent);
     }
 }
