56f8e3d25ab3a60249bde91938d74271f3c05076
Alina Shah <38914105+alinamon@users.noreply.github.com>
2018-08-26 06:32:09 +0200

    espresso test for creating cool note

---
+++ client/app/src/androidTest/java/edu/kit/pse/fridget/client/activity/espresso/CreateTextCoolNoteTest.java
@@ -0,0 +1,113 @@
+package edu.kit.pse.fridget.client.activity.espresso;
+
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.drawable.ColorDrawable;
+import android.preference.PreferenceManager;
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.espresso.matcher.BoundedMatcher;
+import android.support.test.internal.util.Checks;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+import android.widget.ImageView;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import edu.kit.pse.fridget.client.R;
+import edu.kit.pse.fridget.client.activity.HomeActivity;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class CreateTextCoolNoteTest {
+
+    @Rule
+    public ActivityTestRule<HomeActivity> mActivityTestRule = new ActivityTestRule<>(HomeActivity.class);
+
+    @Test
+    public void createTextCoolNoteTest() {
+
+        Context context = getInstrumentation().getTargetContext();
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        String magnetColor = sharedPreferences.getString("ownMagnetColor", "N/A");
+
+        ViewInteraction plusButton = onView(withId(R.id.plus_button));
+        plusButton.perform(click());
+
+        ViewInteraction enterTitle = onView(withId(R.id.enterTitle));
+        enterTitle.perform(click());
+        enterTitle.perform(replaceText("testTitle"), closeSoftKeyboard());
+        enterTitle.check(matches(withText("testTitle")));
+
+        ViewInteraction enterContent = onView(withId(R.id.enterContent));
+        enterContent.perform(click());
+        enterContent.perform(replaceText("testContent"), closeSoftKeyboard());
+        enterContent.check(matches(withText("testContent")));
+
+        ViewInteraction postButton = onView(withId(R.id.postButton));
+        postButton.perform(click());
+
+        ViewInteraction title = onView(withId(R.id.title2));
+        title.check(matches(withText("testTitle")));
+
+        ViewInteraction content = onView(withId(R.id.content2));
+        content.check(matches(withText("testContent\n\n")));
+
+        ViewInteraction magnet = onView(withId(R.id.magnet_CN));
+        //magnet.check(matches(withBackgroundTint(Color.parseColor("#" + magnetColor))));
+
+        ViewInteraction backButton = onView(withId(R.id.back_button));
+        backButton.perform(click());
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+
+    public static Matcher<View> withBackgroundTint(final int color) {
+        Checks.checkNotNull(color);
+        return new BoundedMatcher<View, ImageView>(ImageView.class) {
+            @Override
+            public boolean matchesSafely(ImageView magnet) {
+                return color == ((ColorDrawable)magnet.getBackground()).getColor();
+            }
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("with text color: ");
+            }
+        };
+    }
+}
